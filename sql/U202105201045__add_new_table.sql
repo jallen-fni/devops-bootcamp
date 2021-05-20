2f9fb9a0ee6f196f995f5dde6c9fc40352bc1912
Jameson Allen <jallen@fni-stl.com>
2021-05-20 16:11:47 +0000

    goody

---
+++ sql/U202105201045__add_new_table.sql
@@ -0,0 +1,1 @@
+DROP TABLE bobby_drop_tables;
---
+++ sql/V202105201045__add_new_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE bobby_drop_tables (
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR(10),
+    description VARCHAR(50),
+    sometype INTEGER
+);
