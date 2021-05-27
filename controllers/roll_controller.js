80e870e8e85ffb80c31da778b1503978ca402c95
Jameson Allen <jallen@fni-stl.com>
2021-05-27 08:55:24 -0500

    Fixing Tests and removing Feature Toggle

--- controllers/roll_controller.js
+++ controllers/roll_controller.js
@@ -1,31 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Die = require('../models/die');
-const { db } = require('../lib/db');
-const { isEnabled,  } = require('unleash-client');
+const Die = require("../models/die");
+const { db } = require("../lib/db");
+const { isEnabled } = require("unleash-client");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   let die = new Die(req.query.sides);
   let roll = die.roll();
   let message = `A die with ${die.sides} sides just rolled a ${roll}.`;
   await saveRoll(die.sides, roll);
 
-  if (isEnabled('awesome-feature-5')) {
-    let die2 = new Die(req.query.sides);
-    let roll2 = die2.roll();
-    message = `Two dice each with ${die.sides} sides just rolled a ${roll} and ${roll2}.`;
-    await saveRoll(die2.sides, roll2);
-  }
+  let die2 = new Die(req.query.sides);
+  let roll2 = die2.roll();
+  message = `Two dice each with ${die.sides} sides just rolled a ${roll} and ${roll2}.`;
+  await saveRoll(die2.sides, roll2);
 
-  res.send(message)
+  res.send(message);
 });
-
 
 let saveRoll = async function (sides, rollResult) {
   try {
-    await db.none(`INSERT INTO roll_history(die_sides, result) VALUES (${sides}, ${rollResult});`);
+    await db.none(
+      `INSERT INTO roll_history(die_sides, result) VALUES (${sides}, ${rollResult});`
+    );
   } catch (e) {
-    console.log('Failed to log roll history.');
+    console.log("Failed to log roll history.");
     console.log(e);
   }
 };
